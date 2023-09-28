a92800ff268cb334e01728ea5a47bfc8ffd21b19
Minami-eed <i201916718@gmail.com>
2023-09-27 23:23:31 -0500

    upload project

---
+++ src/main/java/com/cibertec/dawiicl1/DawiiCl1Application.java
@@ -0,0 +1,13 @@
+package com.cibertec.dawiicl1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/dawiicl1/DawiiCl1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.dawiicl1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiCl1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
